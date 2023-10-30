7ec3077b04215d729283479927baf933737d3514
the-original-h-bomb <127529152+the-original-h-bomb@users.noreply.github.com>
2023-10-30 16:25:03 -0400

    Update export_slim.py

--- Export_Scripts/export_slim.py
+++ Export_Scripts/export_slim.py
@@ -2,7 +2,7 @@ import snowflake.connector
 import subprocess
 import os
 
-# Snowflake connection parameters
+# Retrieve secrets from GitHub Actions environment variables
 account = os.environ.get("SF_PRD_ACCOUNT")
 user = os.environ.get("SF_PRD_USERNAME")
 password = os.environ.get("SF_PRD_PASSWORD")
@@ -18,17 +18,22 @@ conn = snowflake.connector.connect(
     database=database
 )
 
+# Specify the folder path within your repository
+folder_path = 'Garden_Plants_Export'
+
 # Export DDL to a file
 ddl_query = 'SHOW TABLES;'
-cursor = conn.cursor()
+cursor = con.cursor()
 cursor.execute(ddl_query)
 ddl_results = cursor.fetchall()
-with open('ddl_export.sql', 'w') as ddl_file:
+ddl_file_path = os.path.join(folder_path, 'ddl_export.sql')
+
+with open(ddl_file_path, 'w') as ddl_file:
     for row in ddl_results:
         ddl_file.write(row[0] + '\n')
 
 # Commit to GitHub
 # Replace 'commit message' with your desired commit message
-subprocess.call(['git', 'add', 'ddl_export.sql'])
+subprocess.call(['git', 'add', ddl_file_path])
 subprocess.call(['git', 'commit', '-m', 'commit message'])
 subprocess.call(['git', 'push'])
