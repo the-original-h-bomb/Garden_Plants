e528ef3edb8f9decd6ee004e34fb7cb95fb5f423
the-original-h-bomb <127529152+the-original-h-bomb@users.noreply.github.com>
2023-11-01 14:15:06 -0400

    Update export_regularfit.py

--- Export_Scripts/export_regularfit.py
+++ Export_Scripts/export_regularfit.py
@@ -51,7 +51,7 @@ db_query = f"select * from information_schema.databases where database_NAME not 
 # Execute the query to fetch all databases
 cursor.execute(db_query)
 
-# Fetch all the databases
+###### Fetch all the databases
 databases = cursor.fetchall()
 
 for db in databases:
@@ -70,7 +70,7 @@ for db in databases:
     subprocess.call(['git', 'add', db_ddl_export_path])
     subprocess.call(['git', 'commit', '-m', f'Commit {db_name} DDL'])
 
-    # Export schemas
+###### Export schemas
     schema_query = f"select * from  {db_name}.information_schema.schemata " \
                    f"where schema_name not in ('SCHEMACHANGE', 'INFORMATION_SCHEMA')"
     cursor.execute(schema_query)
@@ -88,7 +88,7 @@ for db in databases:
         with open(schema_export_path, 'w') as schema_file:
             schema_file.write(schema_create_statement)
     
-        # Export tables
+###### Export tables
         table_query = f"SHOW TABLES IN SCHEMA {db_name}.{schema_name}"
         cursor.execute(table_query)
         tables = cursor.fetchall()
@@ -109,6 +109,27 @@ for db in databases:
             subprocess.call(['git', 'add', table_export_path])
             subprocess.call(['git', 'commit', '-m', f'Commit {table_name} DDL'])
 
+###### Export views
+        view_query = f"SHOW VIEWS IN SCHEMA {db_name}.{schema_name}"
+        cursor.execute(view_query)
+        views = cursor.fetchall()
+
+        for view in views:
+            view_name = view[1]
+            view_folder_path = os.path.join(schema_folder_path, "VIEWS")
+            view_export_path = os.path.join(view_folder_path, view_name + ".sql")
+            view_export_query = f"SELECT GET_DDL('VIEW','{db_name}.{schema_name}.{view_name}')"
+            cursor.execute(view_export_query)
+            view_create_statement = cursor.fetchone()[0]
+
+            with open(view_export_path, 'w') as view_file:
+                view_file.write(view_create_statement)
+            
+            # Commit Views to GitHub
+            subprocess.call(['git', 'add', view_export_path])
+            subprocess.call(['git', 'commit', '-m', f'Commit {view_name} DDL'])
+
+##############    
     # Commit Schemas to GitHub
     subprocess.call(['git', 'add', schema_export_path])
     subprocess.call(['git', 'commit', '-m', f'Commit {schema_name} DDL'])
