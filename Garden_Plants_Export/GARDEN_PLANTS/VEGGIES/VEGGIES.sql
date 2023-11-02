666b0d11be49fb67cd2dfa22288d35a7512c0e5d
GitHub Action <action@github.com>
2023-11-02 20:43:04 +0000

    Update Snowflake DDL

--- Garden_Plants_Export/GARDEN_PLANTS/VEGGIES/VEGGIES.sql
+++ Garden_Plants_Export/GARDEN_PLANTS/VEGGIES/VEGGIES.sql
@@ -2,3 +2,8 @@ create or replace schema VEGGIES;
 
 create or replace tag COST_CENTER COMMENT='cost_center tag'
 ;
+create or replace TABLE LU_SOIL_TYPE (
+	SOIL_TYPE_ID NUMBER(38,0),
+	SOIL_TYPE VARCHAR(15),
+	SOIL_DESCRIPTION VARCHAR(75)
+);
